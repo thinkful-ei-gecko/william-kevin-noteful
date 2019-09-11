d617d76a92167f36a899095b52816b8887267873
William Bae <wbae912@gmail.com>
2019-09-11 09:47:44 -0700

    added routes to app and index

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react'
-import './App.css';
-import Header from './Components/Header';
-import Main from './Components/Main';
-import Sidebar from './Components/Sidebar';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Sidebar from "./Components/Sidebar";
+import FolderList from './Components/FolderList';
+import FolderItem from './Components/FolderItem';
+import NoteList from './Components/NoteList';
+import NoteItem from './Components/NoteItem';
 
 export default class App extends Component {
   constructor(props) {
-    super(props)
-  
-    this.state = {
-       store: props.store
-    }
-  }
+    super(props);
 
-  
-  
+    this.state = {
+      store: props.store
+    };
+  }
 
   render() {
     // console.log(this.state.store);
     return (
       <div className="app">
         <Header />
-        <Sidebar 
-          store={this.state.store}
-        />
-        <Main 
-          store={this.state.store}
-        />
+        <Sidebar>
+          <Route path="/" component={FolderList} />
+          <Route path="/folder/:folderId" component={FolderItem} />
+        </Sidebar>
+        <Main>
+          <Route path="/" component={NoteList} />
+          <Route path="/note/:noteId" component={NoteItem} />
+        </Main>
       </div>
-    )
+    );
   }
 }
