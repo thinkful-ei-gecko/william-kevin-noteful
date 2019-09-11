b3945e9e66f3c5096d9e3b20a8bcfc71f5098c79
William Bae <wbae912@gmail.com>
2019-09-11 14:56:51 -0700

    completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <h1>Noteful</h1>
+                <Link to ='/'>
+                    <h1>Noteful</h1>
+                </Link>
             </div>
         )
     }
